cc6ffd62a7ebb53f7eca40ad51914dc382d15eed
lina mierkE <lina_maria89@hotmail.com>
2020-12-17 23:18:41 -0500

    new url

--- src/components/about.js
+++ src/components/about.js
@@ -11,7 +11,7 @@ export default function About() {
             <div style={{height: '50%', width: '50%'}}>
             <h1> Hello I am Lina! </h1>
             <p> Goal-oriented Software Developer bringing a strong commitment to collaboration and solutions-oriented problem-solving. Leader, enthusiastic, and eager to contribute to team success through hard work, creativity, attention to detail, and excellent organizational skills. Use various web design packages to develop custom-crafted, customer-focused websites, and designs.</p>
-                <img src={girl} alt="girl" className="img" />
+                <img src="https://i.imgur.com/KNheUPa.png" alt="girl" className="img" />
             
             <a href="https://github.com/LinaMierke" target="_blank"> 
             <LinkedInIcon style={{fontSize: 50, padding: '20px'}}/>
