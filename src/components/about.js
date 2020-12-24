48ea123de449b69e91f10a776c0742dacd425885
lina mierkE <lina_maria89@hotmail.com>
2020-12-24 12:58:22 -0500

    lina

--- src/components/about.js
+++ src/components/about.js
@@ -11,8 +11,11 @@ export default function About() {
         <div className="container2" style={{paddingTop:'0'}}>
             <div style={{height: '50%', width: '50%'}}>
             <div className="inner"> 
-            <h1> Hello! I am Lina! </h1>
-            <p> Goal-oriented Software Developer bringing a strong commitment to collaboration and solutions-oriented problem-solving. Leader, enthusiastic, and eager to contribute to team success through hard work, creativity, attention to detail, and excellent organizational skills. Use various web design packages to develop custom-crafted, customer-focused websites, and designs.</p>
+            <h1> Hello! I am Lina </h1>
+            <p> I'm a DC based Software Developer who loves to create, learn and explore. 
+            Being in the Education field shows me the amazing power of knowledge and thanks to that I treat everyday as a new opportunity to grow and improve! I bring hard work, creativity, attention to detail, and excellent organizational skills to my daily work. If you want to know more about me don't hesitate to reach out to me. 
+
+            </p>
             </div> 
                 <img src="https://i.imgur.com/KNheUPa.png" alt="girl" rel="noopener noreferrer" className="img" />
             <div className="social">       
